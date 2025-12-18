46d402e42122ba18e0f159f0efe8fa11ebf9ed70
Cristi Constantin <git@cristi.email>
2025-12-18 14:05:51 +0000

    Updated more dependencies

--- test/record.test.js
+++ test/record.test.js
@@ -6,32 +6,30 @@ import { recordPage } from '../src/record.js';
 const PORT = 12345;
 
 function createTestServer() {
-    const server = express();
-    server.http = http.createServer(server);
-    server.set('etag', false);
-    server.port = PORT;
-    server.url = `http://localhost:${PORT}`;
-    return server;
+    const app = express();
+    const server = http.createServer(app);
+    app.set('etag', false);
+    return { app, server };
 }
 
 test('basic record page', async (t) => {
-    const server = createTestServer();
-    server.get('/', function (_, res) {
+    const { app, server } = createTestServer();
+    app.get('/', function (_, res) {
         res.send('<!DOCTYPE html><html><head><meta charset="utf-8"></head><body>Hello world</body></html>');
     });
-    server.listen(PORT);
+    await new Promise((resolve) => server.listen(PORT, resolve));
 
     const { snapshot, browser } = await recordPage({
-        url: server.url,
+        url: `http://localhost:${PORT}`,
         timeout: 3,
         imgTimeout: 3,
         headless: true,
     });
-    t.is(snapshot.url, server.url);
-    t.is(snapshot.base_url, server.url + '/');
+    t.is(snapshot.url, `http://localhost:${PORT}`);
+    t.is(snapshot.base_url, `http://localhost:${PORT}/`);
     t.true(snapshot.html.includes('<body>Hello world</body>'));
     t.deepEqual(snapshot.responses, {});
 
     await browser.close();
-    server.http.close();
+    server.close();
 });
